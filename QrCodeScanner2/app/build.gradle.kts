bdb3a1be8fe63a6e30916a14bf8d42d98b0fb0f8
maelpaul <maelpaul@orange.fr>
2023-11-03 18:03:08 +0100

    yo

--- QrCodeScanner2/app/build.gradle.kts
+++ QrCodeScanner2/app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 
     implementation ("com.google.android.gms:play-services-vision:20.1.3")
     implementation("com.google.android.gms:play-services-vision-common:19.1.3")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- QrCodeScanner2/app/src/main/java/com/zizou/qrcodescanner/MainActivity.kt
+++ QrCodeScanner2/app/src/main/java/com/zizou/qrcodescanner/MainActivity.kt
@@ -9,6 +9,15 @@ import android.widget.TextView
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import androidx.appcompat.app.AlertDialog
+import android.content.DialogInterface
 
 import java.io.IOException
 import java.net.InetAddress
@@ -17,6 +26,9 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestLocationPermission = 1
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private lateinit var qrCodeValueButton: Button
     private lateinit var startScanButton: Button
@@ -41,6 +53,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if (checkLocationPermission()) {
+            getLocation()
+        }
+
         qrCodeValueButton = findViewById(R.id.qr_code_value_button)
         startScanButton = findViewById(R.id.start_scan_button)
         qrCodeValueButton.isEnabled = false
@@ -53,12 +71,64 @@ class MainActivity : AppCompatActivity() {
             resultLauncher.launch(intent)
         }
 
-        qrCodeValueButton.setOnClickListener() {
+        qrCodeValueButton.setOnClickListener {
             val intent = Intent(this, WebPageActivity::class.java)
             intent.putExtra("urlScanned", qrCodeValueButton.text)
             qrCodeValueButton.isEnabled = false
             resultLauncher.launch(intent)
         }
+    }
+    private fun checkLocationPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), requestLocationPermission)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestLocationPermission && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getLocation()
+        }
+    }
+    private fun getLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                requestLocationPermission
+            )
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+
+                    showLocationDialog(latitude, longitude)
+                }
+            }
+    }
+    private fun showLocationDialog(latitude: Double, longitude: Double) {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("Coordonnées de localisation")
+        alertDialogBuilder.setMessage("Latitude: $latitude\nLongitude: $longitude")
+        alertDialogBuilder.setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+
+        val alertDialog: AlertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 
 }
